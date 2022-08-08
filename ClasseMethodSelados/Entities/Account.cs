72638b152f527d15c18e79b21f02eb7b063ee1b5
Alison <alisson_gamer2000@outlook.com>
2022-08-08 12:13:37 -0300

    add Entities

---
+++ ClasseMethodSelados/Entities/Account.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ClasseMethodSelados.Entities
+{
+    internal class Account
+    {
+        /**
+         * 
+         * O atributo protected ela só da permissao para Classe Mãe
+         * e a subClasse que pertence a ela poder alterar o valores
+         * do atributo
+         * 
+         **/
+        //atributo autoProprietes
+        public int Number { get; protected set; }
+        public string Holder { get; protected set; }
+        public double Balance { get; protected set; }
+
+        //Construtor
+        public Account()
+        {
+        }
+
+        public Account(int number, string holder, double balance)
+        {
+            Number = number;
+            Holder = holder;
+            Balance = balance;
+        }
+
+        //Methods
+        /**
+         * 
+         * Aqui vou usar a palavra "virtual" para
+         * avisar o CSharp que vou querer alterar
+         * o method WithDraw() nas subClasses. Então
+         * vou utilizar a palavra "virtual"
+         * 
+         **/
+        public virtual void WithDraw(double amount)
+        {
+            //add uma taxa pelo saque na conta commum
+            Balance -= amount + 5.0;
+
+        }
+        public void Deposit(double amount)
+        {
+
+            Balance += amount;
+
+        }
+    }
+}
---
+++ ClasseMethodSelados/Entities/SavingsAccount.cs
@@ -0,0 +1,52 @@
+﻿
+namespace ClasseMethodSelados.Entities
+{
+    internal class SavingsAccount : Account
+    {
+        //atributos
+        public double InterestRate { get; set; }
+
+        //construtor
+        public SavingsAccount()
+        {
+        }
+
+        //usando o base() para reaproveitar o construtor da class mãe
+        public SavingsAccount(int number, string holder, double balance, double interestRate)
+            : base(number, holder, balance)
+        {
+
+            InterestRate = interestRate;
+
+        }
+
+        //method
+        public void UpdateBalance()
+        {
+
+            Balance += Balance * InterestRate;
+
+        }
+
+        /**
+         * 
+         * Aqui vou rescrever o method "WithDraw()" para que 
+         * a taxa de 5.00 não seja aplicado na SavingsAccount
+         * para isso tereri que usar a palavra "override" para 
+         * sobrescrever
+         * 
+         **/
+        /*public override void WithDraw(double amount)
+        {
+            Balance -= amount;
+        }*/
+
+        public override void WithDraw(double amount)
+        {
+            base.WithDraw(amount);  //aqui estou chamando o method diretamente na superClasse(Classe mãe), um tipo de reaproveitamento
+            Balance -= 2.0;
+
+        }
+
+    }
+}
