748bc66f4e058cb41f5bd7b3930a469174c64924
Alison <alisson_gamer2000@outlook.com>
2022-08-08 12:08:05 -0300

    add files

---
+++ ClasseMethodSelados/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ClasseMethodSelados
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
